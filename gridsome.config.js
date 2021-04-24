cec89171304c229a06b62eed1926fe735ce2efc6
Tim Janssen <janssentwh@gmail.com>
2021-04-24 12:11:07 +0200

    Initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  siteName: 'Gridsome + Shopify 😍',
+  templates: {
+    ShopifyProduct: [
+      {
+        path: '/product/:handle',
+        component: './src/templates/Product.vue'
+      }
+    ],
+    ShopifyCollection: [
+      {
+        path: '/collection/:handle',
+        component: './src/templates/Collection.vue'
+      }
+    ]
+  },
+  plugins: [
+    {
+      use: 'gridsome-source-shopify',
+      options: {
+        storeName: process.env.GRIDSOME_SHOPIFY_STOREFRONT,
+        storefrontToken: process.env.GRIDSOME_SHOPIFY_STOREFRONT_TOKEN
+      }
+    },
+    {
+      use: 'gridsome-plugin-flexsearch',
+      options: {
+        flexsearch: {
+          profile: 'match'
+        },
+        collections: [
+          {
+            typeName: 'ShopifyProduct',
+            indexName: 'Product',
+            fields: ['title', 'handle', 'description']
+          },
+          {
+            typeName: 'ShopifyCollection',
+            indexName: 'Collection',
+            fields: ['title', 'handle', 'description']
+          }
+        ],
+        searchFields: ['title', 'handle', 'tags']
+      }
+    }
+  ]
+}
